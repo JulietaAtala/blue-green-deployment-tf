74eed95556265811967f0558c9728db9ed931363
atala89549 <julieta_atala@epam.com>
2025-10-02 10:29:48 -0300

    kwdf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,42 @@
 variable "project_prefix" {
   description = "Project identifier used for resource naming and tagging."
+  type        = string
+
 }
 
 variable "aws_region" {
   description = "AWS region for resource deployment."
+  type        = string
 }
 
 variable "vpc_name" {
   description = "Name tag of the pre-configured VPC."
+  type        = string
 }
 
 variable "public_subnet1_name" {
   description = "Name tag of the first public subnet."
+  type        = string
 }
 
 variable "public_subnet2_name" {
   description = "Name tag of the second public subnet."
+  type        = string
 }
 
 variable "sg_ssh_name" {
   description = "Name tag of the SSH Security Group."
-  default     = "cmtr-m68g13qx-sg-ssh"
+  type        = string
 }
 
 variable "sg_http_name" {
   description = "Name tag of the HTTP instances Security Group."
+  type        = string
 }
 
 variable "sg_lb_name" {
   description = "Name tag of the Load Balancer Security Group."
+  type        = string
 }
 
 
@@ -44,4 +52,5 @@ variable "green_weight" {
 
 variable "ec2_instance_type" {
   description = "Instance type for EC2 instances in the ASGs."
+  type        = string
 }
